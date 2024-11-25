50bb32417293365549b7aff5c210f8df9f489d24
madhav <eragon.cube@outlook.com>
2024-11-25 21:56:19 +0530

    Setup firebase

---
+++ firebase-admin.ts
@@ -0,0 +1,26 @@
+import {
+  initializeApp,
+  getApp,
+  getApps,
+  App,
+  cert,
+  ServiceAccount,
+} from "firebase-admin/app";
+
+import { getFirestore } from "firebase-admin/firestore";
+
+import serviceKey from "./service_key.json";
+
+let app: App;
+
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert(serviceKey as string | ServiceAccount),
+  });
+} else {
+  app = getApp();
+}
+
+const adminDb = getFirestore(app);
+
+export { app as adminApp, adminDb };
